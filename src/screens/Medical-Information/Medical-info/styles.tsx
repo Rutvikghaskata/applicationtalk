ca34fbd1de72f9adc7a847f0ed448ef52bcfac5a
rutvikghaskata123 <rutik.dvij@gmail.com>
2022-11-10 16:02:10 +0530

    New Changes

--- src/screens/Medical-Information/Medical-info/styles.tsx
+++ src/screens/Medical-Information/Medical-info/styles.tsx
@@ -1,9 +1,8 @@
 import {StyleSheet} from 'react-native';
-import {Colors, Fonts} from '../../../Theme';
+import {Colors} from '../../../Theme';
 import {
   responsiveHeight,
   responsiveWidth,
-  responsiveFontSize,
 } from 'react-native-responsive-dimensions';
 
 export const styles = StyleSheet.create({
