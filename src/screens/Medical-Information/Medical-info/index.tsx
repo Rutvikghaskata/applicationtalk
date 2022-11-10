24b6996ba049adbff96198b51376defb2efdba0e
rutvikghaskata123 <rutik.dvij@gmail.com>
2022-11-10 15:59:04 +0530

    New Changes

--- src/screens/Medical-Information/Medical-info/index.tsx
+++ src/screens/Medical-Information/Medical-info/index.tsx
@@ -1,41 +1,48 @@
-import { SafeAreaView,View, Text } from 'react-native'
-import React from 'react'
+import {SafeAreaView, View, Text} from 'react-native';
+import React from 'react';
 import {styles} from './styles';
-import {BackIcon,CustomText,SelectInput} from '../../../components/Common'
+import {
+  BackIcon,
+  CustomText,
+  SelectInput,
+  Button,
+} from '../../../components/Common';
+import {useAppNavigation} from '../../../navigations/hook';
 
 const MedicalInfo = () => {
+  const navigation = useAppNavigation();
   return (
     <SafeAreaView style={styles.container}>
-      <BackIcon onPress={() =>{console.log()}} style={styles.backButton}/>
-      <View>
-         <CustomText type="medium" style={styles.title}>Medical information</CustomText>
-         <View style={styles.selectWrapper1}>
-            <SelectInput 
-             placeholder="Allergies"
-             icon='nose'
-            /> 
-         </View>
-         <View style={styles.selectWrapper}>
-            <SelectInput 
-             placeholder="Chronic Illness"
-             icon='sickness'
-            /> 
-         </View>
-         <View style={styles.selectWrapper}>
-            <SelectInput 
-             placeholder="Injuries"
-             icon='injury'
-            /> 
-         </View>
-         <View style={styles.selectWrapper}>
-            <SelectInput 
-             placeholder="Surgeries"
-             icon='surgery'
-            /> 
-         </View>
+      <BackIcon
+        onPress={() => {
+          console.log();
+        }}
+        style={styles.backButton}
+      />
+      <View style={styles.content}>
+        <View>
+          <CustomText type="medium" style={styles.title}>
+            Medical information
+          </CustomText>
+          <View style={styles.selectWrapper1}>
+            <SelectInput placeholder="Allergies" icon="nose" />
+          </View>
+          <View style={styles.selectWrapper}>
+            <SelectInput placeholder="Chronic Illness" icon="sickness" />
+          </View>
+          <View style={styles.selectWrapper}>
+            <SelectInput placeholder="Injuries" icon="injury" />
+          </View>
+          <View style={styles.selectWrapper}>
+            <SelectInput placeholder="Surgeries" icon="surgery" />
+          </View>
+        </View>
+        <View>
+          <Button label="Next" onPress={()=>{navigation.navigate('lifeStyleInfo')}}/>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default MedicalInfo
+export default MedicalInfo;
--- src/screens/Medical-Information/Medical-info/styles.tsx
+++ src/screens/Medical-Information/Medical-info/styles.tsx
@@ -7,19 +7,25 @@ import {
 } from 'react-native-responsive-dimensions';
 
 export const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     paddingHorizontal: responsiveWidth(5),
     backgroundColor: Colors.white,
   },
   backButton: {marginTop: 20, height: 18},
-  title:{
+  content: {
+    height: responsiveHeight(100) - 40,
+    justifyContent:'space-between',
+    paddingHorizontal:responsiveWidth(1),
+    paddingBottom:responsiveWidth(6)
+  },
+  title: {
     alignSelf: 'center',
   },
-  selectWrapper:{
+  selectWrapper: {
     marginTop: responsiveHeight(3),
   },
-  selectWrapper1:{
+  selectWrapper1: {
     marginTop: responsiveHeight(7),
-  }
+  },
 });
