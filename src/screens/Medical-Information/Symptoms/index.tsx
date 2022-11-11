7b6bcd33c3acbecee7f1cc4906e61a08981be7e8
dharakachhdia <dhara.dvij@gmail.com>
2022-11-11 12:23:46 +0530

    worked on symptoms scrren

--- src/screens/Medical-Information/Symptoms/index.tsx
+++ src/screens/Medical-Information/Symptoms/index.tsx
@@ -1,15 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import {styles} from './styles';
-import {SafeAreaView, Text, View, TextInput} from 'react-native';
-import {BackIcon, CustomText,Button} from '../../../components/Common';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  ScrollView,
+} from 'react-native';
+import {BackIcon, CustomText, Button} from '../../../components/Common';
 import CheckBox from '@react-native-community/checkbox';
 import {useAppNavigation} from '../../../navigations/hook';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Feather from 'react-native-vector-icons/Feather';
 const Symptoms = () => {
   const initialState = {
-    react: false,
-    next: false,
-    vue: false,
-    angular: false,
+    Fever: false,
+    Headache: false,
+    BackPain: false,
+    KneePains: false,
+    Diabetics: false,
+    General: false,
+    BodyPain: false,
+    ShoulderPain: false,
+    LegPain: false,
+    NeckPain: false,
+    InternalPain: false,
+    Other: false,
   };
 
   const navigation = useAppNavigation();
@@ -18,6 +35,7 @@ const Symptoms = () => {
   const [masterDataSource, setMasterDataSource] = useState([]);
   const [state, setState] = React.useState(initialState);
   const [toggleButton, setToggleButton] = React.useState(false);
+  const [check, setCheck] = React.useState(false);
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
@@ -82,184 +100,247 @@ const Symptoms = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <BackIcon
-        onPress={() => {
-          console.log();
-        }}
-        style={styles.backButton}
-      />
-      <View style={styles.content}>
+      <View style={styles.uperScroll}>
+        <View style={styles.containerMain}>
+          <BackIcon
+            onPress={() => {
+              console.log();
+            }}
+            style={styles.backButton}
+          />
+        </View>
+
         <View>
           <CustomText type="medium" style={styles.title}>
             Symptoms
           </CustomText>
-          <View style={styles.mainContent}>
+        </View>
+        <View style={styles.containerSearch}>
+          <View style={styles.serachInputStyle}>
+            <Feather
+              name="search"
+              size={20}
+              color="#808080"
+              style={styles.iconInputStyle}
+            />
             <TextInput
               style={styles.textInputStyle}
               onChangeText={text => searchFilterFunction(text)}
               value={search}
               underlineColorAndroid="transparent"
               placeholder="Search Symptoms"
+              placeholderTextColor="#808080"
             />
-            {/* <FlatList
+          </View>
+          {/* <FlatList
           data={filteredDataSource}
           keyExtractor={(item, index) => index.toString()}
           ItemSeparatorComponent={ItemSeparatorView}
           renderItem={ItemView}
         /> */}
-          </View>
-          <View>
-            <View style={styles.mainContentTitle}></View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                // onFillColor="#0094DE"
-                value={state.react}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    react: value,
-                  })
-                }
-              />
-              <Text>Fever</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.next}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    next: value,
-                  })
-                }
-              />
-              <Text>Headache</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.vue}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    vue: value,
-                  })
-                }
-              />
-              <Text>Back Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Knee Pains</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Diabetics</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>General</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Body Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Shoulder Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Leg Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Neck Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Internal Pain</Text>
-            </View>
-            <View style={styles.checkboxWrapper}>
-              <CheckBox
-                value={state.angular}
-                onValueChange={value =>
-                  setState({
-                    ...state,
-                    angular: value,
-                  })
-                }
-              />
-              <Text>Other</Text>
-            </View>
-          </View>
-          <View>
-            <Button label="Next" onPress={() => onHandleNavigate('symptoms')} />
-          </View>
         </View>
+      </View>
+      <View style={styles.selctionContainer}>
+        <View style={styles.mainContentTitle}>
+          <Text style={styles.subtitle}>Select Symptoms to add</Text>
+        </View>
+        <ScrollView     showsHorizontalScrollIndicator={true} style={styles.containerCheckbox}>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.Fever}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  Fever: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Fever</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.Headache}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  Headache: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Headache</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.BackPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  BackPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Back Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.KneePains}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  KneePains: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Knee Pains</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.Diabetics}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  Diabetics: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Diabetics</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.General}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  General: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>General</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.BodyPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  BodyPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Body Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.ShoulderPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  ShoulderPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Shoulder Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.LegPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  LegPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Leg Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.NeckPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  NeckPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Neck Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.InternalPain}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  InternalPain: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Internal Pain</Text>
+          </View>
+          <View style={styles.checkboxWrapper}>
+            <CheckBox
+              tintColors={{false: '#BABABA'}}
+              onFillColor="#0094DE"
+              onCheckColor={'#0094DE'}
+              onTintColor={'#0094DE'}
+              value={state.Other}
+              onValueChange={value =>
+                setState({
+                  ...state,
+                  Other: value,
+                })
+              }
+            />
+            <Text style={styles.checkboxText}>Other</Text>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.ButtonContainer}>
+        <Button label="Next" onPress={() => onHandleNavigate('symptoms')} />
       </View>
     </SafeAreaView>
   );
