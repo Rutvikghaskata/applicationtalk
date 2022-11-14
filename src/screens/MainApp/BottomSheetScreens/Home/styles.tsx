d1027810311c5fc5895d9fc3f7e190d0a7b3dc3a
dharakachhdia <dhara.dvij@gmail.com>
2022-11-14 12:51:07 +0530

    new

--- src/screens/MainApp/BottomSheetScreens/Home/styles.tsx
+++ src/screens/MainApp/BottomSheetScreens/Home/styles.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet} from 'react-native';
-import {Colors} from '../../../../Theme';
+import {Colors,Fonts} from '../../../../Theme';
 import {
   responsiveHeight,
   responsiveWidth,
